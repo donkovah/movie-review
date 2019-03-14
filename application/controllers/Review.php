17b44cb33adbe6459cfdd60d101216edef9c5753
Oluwasegun Kesington <iamkesington@gmail.com>
2019-03-15 00:39:27 +0100

    Create Auth Route

--- application/controllers/Review.php
+++ application/controllers/Review.php
@@ -6,15 +6,17 @@ class Review extends CI_Controller {
 	public function __construct()
 	{
 		parent::__construct();
+		$this->load->database();
 		$this->lang->load('auth');
+		$this->load->library(['ion_auth', 'form_validation']);		
 		$this->load->model('reviews_model');
         $this->load->helper('url_helper');
 	}
 
 	public function index()
 	{
-		$data['reviews'] = $this->reviews_model->get_reviews();
-		$this->load->view('dashboard');
+        $data['reviews'] = $this->reviews_model->get_reviews();
+		$this->load->view('dashboard', $data);
 	}
 
 	public function view($id = NULL)
@@ -24,6 +26,16 @@ class Review extends CI_Controller {
   //           show_404();
   //       }
 		$this->load->view('reviews/comments/index', $data);
+	}
+
+
+	public function create()
+	{
+		if (!$this->ion_auth->logged_in()){
+			redirect('auth/login', 'refresh');
+		}
+		var_dump('request');
+		die();
 	}
 
 	public function storeReview($request)
